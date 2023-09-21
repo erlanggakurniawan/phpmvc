35619159cb0e10027b2d22e44045ea3cfed3e22c
erlanggakurniawan <erlanggakurniawan989@gmail.com>
2023-09-21 14:25:52 +0700

    php mvc

---
+++ app/controllers/mahasiswa.php
@@ -0,0 +1,49 @@
+<?php
+
+class mahasiswa extends Controller{
+    public function index()
+    {
+        $data['judul'] = 'Daftar Mahasiswa';
+        $data['mhs'] = $this->model('mahasiswa_model')->getALLmahasiswa();
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Mahasiswa';
+        $data['mhs'] = $this->model('mahasiswa_model')->getMahasiswaById($id);
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
+    
+    public function tambah()
+    {
+        if($this->model('mahasiswa_model')->tambahDataMahasiswa($_POST) > 0){
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+
+    public function hapus($id)
+    {
+        if($this->model('mahasiswa_model')->hapusDataMahasiswa($id) > 0){
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
+            header('Location:' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+}
+
+?>
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+        <H1 class="mt-4">About Me</H1>
+        <img src="<?= BASEURL; ?>/img/WhatsApp Image 2023-08-25 at 7.42.46.jpeg" alt="Angga" width="200" class="rounded-circle shadow">
+        <p>Halo, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> tahun, Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
